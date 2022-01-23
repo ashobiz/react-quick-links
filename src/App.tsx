c2d27a5a3b91e5004ec44e278bcbf499cc80c413
Ashok <ashobiz@gmail.com>
2022-01-23 10:31:27 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  // Download test test test
+  // Download test test test download
   return (
     <div className="App">
       <header className="App-header">

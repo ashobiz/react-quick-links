be588eb0b4a7e28a1f37de0167a6002f1c2793bb
Ashok <ashobiz@gmail.com>
2022-01-29 17:21:15 +0530

    Added different styles. Added Styling for menu links (block, pills and icon). Added main button close icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,49 @@
 import React from 'react';
 import Main from './Main';
-import { FaBars, FaAmazon } from 'react-icons/fa';
+import { FaBars, FaAmazon, FaApple, FaTimes } from 'react-icons/fa';
 import MenuItem from './MenuItem';
 
 const App: React.FC = () => {
   // Options for menu
   const options = {
     button: {
-      buttonName: <FaBars />,
+      buttonName: <FaBars />, // Icon or string
+      buttonClose: <FaTimes />, // Icon or string
       backgroundColor: '#ff0000',
       textColor: 'white',
-      rounded: true,
+      rounded: true, // boolean
+    },
+    menuItems: {
+      style: 'block', // pills, block, icon
     },
   };
 
   return (
     <>
-      {/* buttonName can be either String or React component (in case of icons) */}
       <Main options={options}>
         <MenuItem
-          linkText={'Hello'}
-          linkIcon={<FaAmazon />}
-          linkHref={'/hello'}
+          linkText={'Hello downlaod'} // Display text
+          linkHref={'/hello'} // Link href
+          linkIcon={<FaAmazon />} // Icon, if needed
+          linkIconColor="pink" // Icon color
         />
         <MenuItem
           linkText={'Hello'}
-          linkIcon={<FaAmazon />}
           linkHref={'/hello'}
+          linkIcon={<FaApple />}
+          linkIconColor={'red'}
         />
         <MenuItem
           linkText={'Hello'}
-          linkIcon={<FaAmazon />}
           linkHref={'/hello'}
+          linkIcon={<FaAmazon />}
+          linkIconColor={'blue'}
         />
         <MenuItem
           linkText={'Hello'}
-          linkIcon={<FaAmazon />}
           linkHref={'/hello'}
+          linkIcon={<FaAmazon />}
+          linkIconColor={'orange'}
         />
       </Main>
     </>
